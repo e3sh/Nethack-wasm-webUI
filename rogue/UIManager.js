866b9422606f424185de4904407025254bc1a8f2
Eizo Shimada <e3shimada@gmail.com>
2026-02-10 21:55:44 +0900

    Update
    翻訳追加

--- rogue/UIManager.js
+++ rogue/UIManager.js
@@ -488,7 +488,8 @@ function UIManager(r, g) {
                         const charStr = (item.identifier !== 0 && item.ch != String.fromCharCode(0)) ? (typeof item.ch === 'string' ? item.ch : String.fromCharCode(item.ch)) + ")" : " ";
                         const glyph = (item.glyph) ? (((item.glyph.glyph > 255) && (item.glyph.glyph < 4000)) ? String.fromCharCode(item.glyph.glyph + d.GLYPH_BASE) : " ") : " ";
                         const textStr = this.trancelate.message(item.str);
-                        this.mvwaddch(menuDsp, (i - start) + 1, 0, `${prefix}${charStr}${glyph}${textStr}`);
+                        const guide = (item.identifier == 0 && item.str != "" ? "#":"");
+                        this.mvwaddch(menuDsp, (i - start) + 1, 0, `${prefix}${charStr}${glyph}${guide}${textStr}`);
                     }
 
 
